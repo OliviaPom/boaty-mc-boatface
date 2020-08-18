b73e5fac042f788fb8241b87feead79d4a7e4d2b
Priya Biring <priya.biring@gmail.com>
2020-08-18 11:53:00 +0100

    changes to edit and update

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,18 @@
 class BoatsController < ApplicationController
+  def edit
+    @boat = Boat.find(params[:id])
+  end
+
+  def update
+    @boat.update(boat_params)
+
+    redirect_to boat_path(@boat)
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name)
+    params.require(:boat).permit(:brand, :price, :location, :name, :category)
+  end
 end
