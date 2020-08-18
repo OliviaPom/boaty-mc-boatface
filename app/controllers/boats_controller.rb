4363b84c0dadd4a63525e2e468e6d9050652dc81
Cameron Rouse <cam-rouse@hotmail.co.uk>
2020-08-18 11:50:44 +0100

    boats-show-destroy

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,20 @@
 class BoatsController < ApplicationController
+
+def show
+  @boat = Boat.find(params[:id])
+end
+
+def destroy
+ @boat = Boat.find(params[:id])
+ @boat.destroy
+ redirect_to boats_path
+end
+
+
+private
+
+def boat_params
+params.require(:boat).permit(:brand, :price, :location, :category, :name)
+end
+
 end
